7b28412f3b584f370a7fb0998c59a5dce858cef9
AztekinLab <jixing.zhong@epfl.ch>
2024-04-15 16:09:57 +0200

    add docs for rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PSutils'
+project = 'coolname'
 copyright = '2024, Jixing Zhong'
 author = 'Jixing Zhong'
 release = '1.0.0'
